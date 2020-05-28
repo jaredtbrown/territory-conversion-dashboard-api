7662f317f1637b56e0a8890b143b14cc0d07acaf
Jared Brown <brown.jared25@gmail.com>
2020-05-28 01:26:28 -0400

    Update TerritoryReportModel.cs

--- TerritoryConversionDashboard/Models/TerritoryReportModel.cs
+++ TerritoryConversionDashboard/Models/TerritoryReportModel.cs
@@ -3,7 +3,7 @@
     public class TerritoryReportModel
     {
         public int Total { get; set; }
-        public int TotatCompleted { get; set; }
+        public int TotalCompleted { get; set; }
         public int TotalInProgress { get; set; }
         public int TotalRemaining { get; set; }
     }
