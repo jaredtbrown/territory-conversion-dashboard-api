aa5e1711bbe11bba9d822cbe9af9314aa80a5d91
Jared Brown <brown.jared25@gmail.com>
2021-01-04 03:21:57 -0500

    Migrate to google cloud functions (#6)
    * Added report function
    
    * Removed api controller, program file, old startup
    
    * gcloud deploy
    
    * Update
    
    * Update
    
    * update
    
    * Update
    
    * update
    
    * Logging
    
    * Fixed it?
    
    * project name
    
    * Renamed function endpoint
    
    * Pipeline yml update
    
    * Injecting logger

---
+++ TerritoryConversionDashboard/Functions/ReportFunction.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Google.Cloud.Functions.Framework;
+using Google.Cloud.Functions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+
+namespace TerritoryConversionDashboard.Functions
+{
+    [FunctionsStartup(typeof(Startup))]
+    public class ReportFunction : IHttpFunction
+    {
+        private readonly IReportService _reportService;
+        private readonly ILogger _logger;
+
+        public ReportFunction(IReportService reportService, ILogger logger)
+        {
+            _reportService = reportService;
+            _logger = logger;
+        }
+
+        public async Task HandleAsync(HttpContext context)
+        {
+            try
+            {
+                var response = context.Response;
+                switch (context.Request.Method)
+                {
+                    case "GET":
+                        var report = await _reportService.GetAsync();
+                        response.StatusCode = (int)HttpStatusCode.OK;
+                        response.ContentType = "application/json";
+                        await response.WriteAsync(JsonConvert.SerializeObject(report));
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                        break;
+                }
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+            }
+        }
+    }
+}
