f628d8931dc0006ba47f633032d7c75aae0d925d
Jared Brown <brown.jared25@gmail.com>
2021-01-04 02:28:22 -0500

    Logging

--- TerritoryConversionDashboard/Functions/ReportFunction.cs
+++ TerritoryConversionDashboard/Functions/ReportFunction.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
 using Google.Cloud.Functions.Framework;
 using Google.Cloud.Functions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using System;
 
 namespace TerritoryConversionDashboard.Functions
 {
@@ -14,6 +13,7 @@ namespace TerritoryConversionDashboard.Functions
     public class ReportFunction : IHttpFunction
     {
         private readonly IReportService _reportService;
+        private readonly ILogger _logger;
 
         public ReportFunction(IReportService reportService)
         {
@@ -22,19 +22,25 @@ namespace TerritoryConversionDashboard.Functions
 
         public async Task HandleAsync(HttpContext context)
         {
-
-            var response = context.Response;
-            switch (context.Request.Method)
+            try
             {
-                case "GET":
-                    var report = await _reportService.GetAsync();
-                    response.StatusCode = (int)HttpStatusCode.OK;
-                    response.ContentType = "application/json";
-                    await response.WriteAsync(JsonConvert.SerializeObject(report));
-                    break;
-                default:
-                    response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
-                    break;
+                var response = context.Response;
+                switch (context.Request.Method)
+                {
+                    case "GET":
+                        var report = await _reportService.GetAsync();
+                        response.StatusCode = (int)HttpStatusCode.OK;
+                        response.ContentType = "application/json";
+                        await response.WriteAsync(JsonConvert.SerializeObject(report));
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                        break;
+                }
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
             }
         }
     }
